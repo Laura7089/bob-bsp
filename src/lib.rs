e5342a9504546454ec8f8a7beef0ffdec431daa7
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2023-06-18 17:46:42 +0100

    chore: add a big ol' list of TODOs and dependencies on cortex-m

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,16 @@
 #![forbid(unsafe_code)]
 #![deny(clippy::pedantic)]
 
+// TODO: magnetometer
+// TODO: altimeter
+// TODO: accelerometer
+// TODO: buzzer
+// TODO: GPIO breakout
+// TODO: i2c (since the contacts are exposed)
+// TODO: flash (perhaps an embedded-storage impl?)
+// TODO: pass through cortex-m{,-rt} crates?
+// TODO: pyro channels
+
 #[cfg(test)]
 mod tests {
     use super::*;
