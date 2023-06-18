49c7231a1e08ec803a1767f822c92e91f3545682
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2023-06-18 19:42:53 +0100

    refactor: add docs for some stuff, rework get_sensors

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
+//! Board support package for [Bob](https://github.com/yorkaerospace/Bob) revision 3.
+//!
+//! The intention with this crate is that all the peripherals available on Bob as a board are
+//! available to software, and anything that is not available by default is discarded.
+//!
+//! Essentially, this is a wrapper around [`rp2040_hal`] and the crates for the sensors available
+//! on Bob.
 #![no_std]
 #![forbid(unsafe_code)]
+#![deny(missing_docs)]
 #![deny(clippy::pedantic)]
 
 use core::cell::RefCell;
@@ -11,8 +19,8 @@ use rp2040_hal::gpio::{bank0, FunctionI2C, Pin};
 pub extern crate hp203b;
 pub extern crate rp2040_hal;
 
-/// i2c0 as exposed on bob
-pub type I2C = rp2040_hal::I2C<
+/// `i2c0` as exposed on bob
+pub type I2C0 = rp2040_hal::I2C<
     rp2040_hal::pac::I2C0,
     (
         Pin<bank0::Gpio16, FunctionI2C>,
@@ -20,12 +28,9 @@ pub type I2C = rp2040_hal::I2C<
     ),
 >;
 
-type I2CShared<'a> = embedded_hal_bus::i2c::RefCellDevice<'a, I2C>;
-
-pub type Altimeter<'a, M> = hp203b::HP203B<I2CShared<'a>, M, hp203b::csb::CSBHigh>;
-
 type PinDefault<T> = rp2040_hal::gpio::Pin<T, <T as rp2040_hal::gpio::pin::PinId>::Reset>;
 
+#[allow(missing_docs)]
 /// Exposed breakout pins on bob
 pub struct Gpio {
     pub gpio0: PinDefault<bank0::Gpio0>,
@@ -39,19 +44,24 @@ pub struct Gpio {
     pub gpio29: PinDefault<bank0::Gpio29>,
 }
 
-// TODO: magnetometer
-// TODO: accelerometer
-// TODO: buzzer
-// TODO: flash (perhaps an embedded-storage impl?)
-// TODO: pyro channels
+/// Singleton struct for peripherals available on Bob
+#[allow(missing_docs)]
 pub struct Peripherals {
-    pub i2c: RefCell<I2C>,
+    pub i2c0: RefCell<I2C0>,
     pub gpio: Gpio,
-    alti_taken: bool,
+    // TODO: magnetometer
+    // TODO: accelerometer
+    // TODO: buzzer
+    // TODO: flash (perhaps an embedded-storage impl?)
+    // TODO: pyro channels
+    // TODO: can we do anything with the screw terminal?
 }
 
 impl Peripherals {
     // TODO: pass through i2c settings
+    /// Take ownership of peripherals
+    ///
+    /// This can only be called once.
     pub fn take() -> Self {
         let mut perips_base = rp2040_hal::pac::Peripherals::take().unwrap();
 
@@ -63,7 +73,7 @@ impl Peripherals {
             &mut perips_base.RESETS,
         );
 
-        let i2c = RefCell::new(rp2040_hal::I2C::i2c0(
+        let i2c0 = RefCell::new(rp2040_hal::I2C::i2c0(
             perips_base.I2C0,
             pins.gpio16.into_mode(),
             pins.gpio17.into_mode(),
@@ -73,7 +83,7 @@ impl Peripherals {
         ));
 
         Self {
-            i2c,
+            i2c0,
             gpio: Gpio {
                 gpio0: pins.gpio0,
                 gpio1: pins.gpio1,
@@ -85,26 +95,33 @@ impl Peripherals {
                 gpio28: pins.gpio28,
                 gpio29: pins.gpio29,
             },
-            alti_taken: false,
         }
     }
+}
 
-    pub fn get_altimeter(
-        &mut self,
-        osr: hp203b::OSR,
-        channel: hp203b::Channel,
-    ) -> Result<
-        Altimeter<'_, hp203b::mode::Pressure>,
-        <I2CShared as embedded_hal::i2c::ErrorType>::Error,
-    > {
-        if self.alti_taken {
-            panic!("Tried to take altimeter more than once");
-        }
-        let new_bus = I2CShared::new(&self.i2c);
-        self.alti_taken = true;
-        let alti = hp203b::HP203B::new(new_bus, osr, channel)?;
-        Ok(alti)
-    }
+type I2C0SharedRC<'a> = embedded_hal_bus::i2c::RefCellDevice<'a, I2C0>;
+
+/// The [`hp203b::HP203B`] as it appears on Bob, using `RefCell` bus sharing
+pub type AltimeterRC<'a, M> = hp203b::HP203B<I2C0SharedRC<'a>, M, hp203b::csb::CSBHigh>;
+
+/// Initialise all onboard sensors with [`embedded_hal_bus::i2c::RefCellDevice`]
+///
+/// This is a single method - it can only be called once.
+/// Returns a tuple of `(altimeter, accelerometer, magnetometer)`.
+// TODO: make a singleton somehow
+pub fn get_sensors(
+    i2c0: &RefCell<I2C0>,
+    alti_osr: hp203b::OSR,
+    alti_channel: hp203b::Channel,
+) -> Result<
+    (AltimeterRC<hp203b::mode::Pressure>,),
+    <I2C0SharedRC as embedded_hal::i2c::ErrorType>::Error,
+> {
+    let alti = {
+        let new_bus = I2C0SharedRC::new(i2c0);
+        hp203b::HP203B::new(new_bus, alti_osr, alti_channel)?
+    };
+    Ok((alti,))
 }
 
 #[cfg(test)]
