48a5e39cc9641a26a67c11b608a34f49cbb4f674
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2023-06-19 01:55:13 +0100

    refactor: remove Peripherals, use bsp_pins macro, add Buzzer

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,6 @@
 #![deny(missing_docs)]
 #![deny(clippy::pedantic)]
 
-use core::cell::RefCell;
-use fugit::RateExtU32;
-use rp2040_hal::gpio::{bank0, FunctionI2C, Pin};
-
 /// The linker will place this boot block at the start of our program image. We
 /// need this to help the ROM bootloader get our code up and running.
 #[cfg(feature = "boot2")]
@@ -22,103 +18,108 @@ use rp2040_hal::gpio::{bank0, FunctionI2C, Pin};
 #[used]
 pub static BOOT2_FIRMWARE: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
-// TODO: pass through cortex-m{,-rt} crates?
+// TODO: magnetometer
+// TODO: accelerometer
+// TODO: buzzer
+// TODO: flash (perhaps an embedded-storage impl?)
+// TODO: pyro channels
+// TODO: can we do anything with the screw terminal?
 
+// TODO: pass through cortex-m{,-rt} crates?
 pub extern crate hp203b;
 pub extern crate rp2040_hal;
 
-/// `i2c0` as exposed on bob
-pub type I2C0 = rp2040_hal::I2C<
-    rp2040_hal::pac::I2C0,
-    (
-        Pin<bank0::Gpio16, FunctionI2C>,
-        Pin<bank0::Gpio17, FunctionI2C>,
-    ),
->;
+use rp2040_hal as hal;
 
-type PinDefault<T> = rp2040_hal::gpio::Pin<T, <T as rp2040_hal::gpio::pin::PinId>::Reset>;
-
-#[allow(missing_docs)]
-/// Exposed breakout pins on bob
-pub struct Gpio {
-    pub gpio0: PinDefault<bank0::Gpio0>,
-    pub gpio1: PinDefault<bank0::Gpio1>,
-    pub gpio2: PinDefault<bank0::Gpio2>,
-    pub gpio3: PinDefault<bank0::Gpio3>,
-    pub gpio4: PinDefault<bank0::Gpio4>,
-    pub gpio26: PinDefault<bank0::Gpio26>,
-    pub gpio27: PinDefault<bank0::Gpio27>,
-    pub gpio28: PinDefault<bank0::Gpio28>,
-    pub gpio29: PinDefault<bank0::Gpio29>,
+hal::bsp_pins! {
+    #[allow(missing_docs)]
+    Gpio0 { name: gpio0 },
+    #[allow(missing_docs)]
+    Gpio1 { name: gpio1 },
+    #[allow(missing_docs)]
+    Gpio2 { name: gpio2 },
+    #[allow(missing_docs)]
+    Gpio3 { name: gpio3 },
+    #[allow(missing_docs)]
+    Gpio4 { name: gpio4 },
+    #[allow(missing_docs)]
+    Gpio26 { name: gpio26 },
+    #[allow(missing_docs)]
+    Gpio27 { name: gpio27 },
+    #[allow(missing_docs)]
+    Gpio28 { name: gpio28 },
+    #[allow(missing_docs)]
+    Gpio29 { name: gpio29 },
+    /// Pin connected to the onboard buzzer
+    Gpio5 {
+        name: buzzer,
+        aliases: { FunctionPwm: BuzzerPwm }
+    },
+    /// Pin used as SDA for [`I2C0`]
+    Gpio16 {
+        name: i2c0_sda,
+        aliases: { FunctionI2C: I2c0Sda }
+    },
+    /// Pin used as SCL for [`I2C0`]
+    Gpio17 {
+        name: i2c0_scl,
+        aliases: { FunctionI2C: I2c0Scl }
+    },
 }
 
-/// Singleton struct for peripherals available on Bob
-#[allow(missing_docs)]
-pub struct Peripherals {
-    pub i2c0: RefCell<I2C0>,
-    pub gpio: Gpio,
-    // TODO: magnetometer
-    // TODO: accelerometer
-    // TODO: buzzer
-    // TODO: flash (perhaps an embedded-storage impl?)
-    // TODO: pyro channels
-    // TODO: can we do anything with the screw terminal?
+type Pwm2 = hal::pwm::Slice<hal::pwm::Pwm2, <hal::pwm::Pwm2 as hal::pwm::SliceId>::Reset>;
+type Pwm2B = hal::pwm::Channel<hal::pwm::Pwm2, hal::pwm::FreeRunning, hal::pwm::B>;
+
+/// Onboard buzzer
+pub struct Buzzer {
+    channel: Pwm2B,
 }
 
-impl Peripherals {
-    // TODO: pass through i2c settings
-    /// Take ownership of peripherals
-    ///
-    /// This can only be called once.
-    pub fn take() -> Self {
-        let mut perips_base = rp2040_hal::pac::Peripherals::take().unwrap();
+use embedded_hal::pwm::SetDutyCycle;
 
-        let sio = rp2040_hal::Sio::new(perips_base.SIO);
-        let pins = rp2040_hal::gpio::Pins::new(
-            perips_base.IO_BANK0,
-            perips_base.PADS_BANK0,
-            sio.gpio_bank0,
-            &mut perips_base.RESETS,
-        );
+impl Buzzer {
+    /// Create a new [`Buzzer`] with a set `frequency`
+    pub fn new(mut pwm: Pwm2, pin: BuzzerPwm, frequency: fugit::KilohertzU64) -> Self {
+        // TODO: what is the clock rate of Bob?
+        pwm.set_div_int(todo!());
+        pwm.set_div_frac(todo!());
 
-        let i2c0 = RefCell::new(rp2040_hal::I2C::i2c0(
-            perips_base.I2C0,
-            pins.gpio16.into_mode(),
-            pins.gpio17.into_mode(),
-            400.kHz(),
-            &mut perips_base.RESETS,
-            125.MHz(),
-        ));
-
-        Self {
-            i2c0,
-            gpio: Gpio {
-                gpio0: pins.gpio0,
-                gpio1: pins.gpio1,
-                gpio2: pins.gpio2,
-                gpio3: pins.gpio3,
-                gpio4: pins.gpio4,
-                gpio26: pins.gpio26,
-                gpio27: pins.gpio27,
-                gpio28: pins.gpio28,
-                gpio29: pins.gpio29,
-            },
-        }
+        let mut channel = pwm.channel_b;
+        channel.output_to(pin);
+        Self { channel }
     }
+
+    // pub fn set_volume(&mut self, vol: u8) -> Result<(), E> {
+    //     let max = self.channel.get_max_duty_cycle()?;
+    //     let desired = (max / 100) * vol as u16;
+    //     self.set_duty_cycle(desired)?;
+    //     Ok(())
+    // }
+
+    // pub fn mute(&mut self) -> Result<(), E> {
+    //     self.set_duty_cycle_fully_off()
+    // }
 }
+
+/// `i2c0` as exposed on Bob
+pub type I2C0 = hal::I2C<hal::pac::I2C0, (I2c0Sda, I2c0Scl)>;
+
+/// [`hp203b::HP203B`] as it appears on Bob
+///
+/// `I` is a generic I2C argument to allow different types of bus sharing.
+/// See [`sensors_rc`] and TODO.
+pub type Altimeter<I, M> = hp203b::HP203B<I, M, hp203b::csb::CSBHigh>;
 
 /// Onboard sensors using [`embedded_hal_bus::i2c::RefCellDevice`]
 pub mod sensors_rc {
     use core::cell::RefCell;
+    use embedded_hal_bus::i2c::RefCellDevice;
 
-    type I2C0Shared<'a> = embedded_hal_bus::i2c::RefCellDevice<'a, super::I2C0>;
-
-    /// The [`hp203b::HP203B`] as it appears on Bob, using `RefCell` bus sharing
-    pub type Altimeter<'a, M> = hp203b::HP203B<I2C0Shared<'a>, M, hp203b::csb::CSBHigh>;
+    type I2C0Shared<'a> = RefCellDevice<'a, super::I2C0>;
 
     /// Initialise all onboard sensors
     ///
-    /// This is a single method - it can only be called once.
+    /// This is a singleton method - it can only be called once.
     /// Returns a tuple of `(altimeter, accelerometer, magnetometer)`.
     // TODO: make a singleton somehow
     pub fn get_sensors(
@@ -126,7 +127,7 @@ pub mod sensors_rc {
         alti_osr: hp203b::OSR,
         alti_channel: hp203b::Channel,
     ) -> Result<
-        (Altimeter<hp203b::mode::Pressure>,),
+        (super::Altimeter<I2C0Shared, hp203b::mode::Pressure>,),
         <I2C0Shared as embedded_hal::i2c::ErrorType>::Error,
     > {
         let alti = {
@@ -138,7 +139,4 @@ pub mod sensors_rc {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-    use test_case::test_case;
-}
+mod tests {}
